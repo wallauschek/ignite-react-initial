6161fe939e04965dba57c1807a02bbf11af8a20c
Pedro Wallauschek <pedro@pedrowallauschek.com.br>
2021-07-13 17:30:32 -0300

    Fast refresh no Webpack e mais alguns conceitos importantes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { Counter } from './components/Counter'
+import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
 
 export function App() {
   // throw new Error('Deu ruim!')
-  return <h1>Hello World!</h1>
+  return (
+    <>
+    <RepositoryList />
+    <Counter />
+    </>
+  )
 }
